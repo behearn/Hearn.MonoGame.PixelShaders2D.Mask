e499964434292dd1a65c31a442b1e40437d14843
Brian Hearn <brian@brianhearn.com>
2018-03-04 12:11:17 +0000

    Initial Commit

---
+++ Hearn.MonoGame.PixelShaders2D.Mask/Content/Mask.fx
@@ -0,0 +1,95 @@
+﻿#if OPENGL
+#define SV_POSITION POSITION
+#define VS_SHADERMODEL vs_3_0
+#define PS_SHADERMODEL ps_3_0
+#else
+#define VS_SHADERMODEL vs_4_0_level_9_1
+#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+Texture2D BaseTexture;
+float BaseWidth;
+float BaseHeight;
+
+Texture2D MaskTexture;
+float MaskWidth;
+float MaskHeight;
+float MaskCenterX;
+float MaskCenterY;
+float MaskScale;
+float MaskRotation; //Radians
+
+sampler2D BaseTextureSampler = sampler_state
+{
+	Texture = <BaseTexture>;
+};
+
+sampler2D MaskTextureSampler = sampler_state
+{
+	Texture = <MaskTexture>;
+};
+
+struct VertexShaderOutput
+{
+	float4 Position : SV_POSITION;
+	float4 Color : COLOR0;
+	float2 TextureCoordinates : TEXCOORD0;
+};
+
+float inBounds(float2 coords, float2 topLeft, float2 bottomRight)
+{
+	//if (coords.x >= topLeft.x && coords.y >= topLeft.y && coords.x < bottomRight.x && coords.y < bottomRight.y)
+	//{
+	//	return 1.0;
+	//}
+	//return 0.0;
+	float2 s = step(topLeft, coords) - step(bottomRight, coords);
+	return s.x * s.y;
+}
+
+float4 Mask(VertexShaderOutput input) : COLOR
+{
+
+	float4 tex = tex2D(BaseTextureSampler, input.TextureCoordinates) * input.Color;
+
+	MaskWidth = MaskWidth * MaskScale;
+	MaskHeight = MaskHeight * MaskScale;
+
+	float maskX = MaskCenterX - (MaskWidth / 2);
+	float maskY = MaskCenterY - (MaskHeight / 2);
+
+	float baseX = input.TextureCoordinates.x * BaseWidth;
+	float baseY = input.TextureCoordinates.y * BaseHeight;
+	
+	float applyMask = inBounds(float2(baseX, baseY), float2(maskX, maskY), float2(maskX + MaskWidth, maskY + MaskHeight));
+
+	float alpha = 0.0;
+
+	float2 maskCoords = float2((baseX - maskX) / MaskWidth, (baseY - maskY) / MaskHeight);
+
+	float s = sin(MaskRotation);
+	float c = cos(MaskRotation);
+
+	float2 oldCoords = maskCoords - 0.5;
+
+	maskCoords.x = clamp((oldCoords.x * c) - (oldCoords.y * s), -0.5, 0.5);
+	maskCoords.y = clamp((oldCoords.x * s) + (oldCoords.y * c), -0.5, 0.5);
+		
+	maskCoords = maskCoords + 0.5;
+		
+	float4 maskTex = tex2D(MaskTextureSampler, maskCoords) * input.Color;
+
+	alpha = maskTex.a * applyMask;
+
+	tex = tex * alpha;
+
+	return tex;
+}
+
+technique Mask
+{
+	pass P0
+	{
+		PixelShader = compile PS_SHADERMODEL Mask();
+	}
+};
---
+++ Hearn.MonoGame.PixelShaders2D.Mask/Game1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Hearn.MonoGame.PixelShaders2D.Mask
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        
+        Texture2D _image;
+        Texture2D _maskTexture;
+        Effect _maskEffect;
+
+        Vector2 _position;
+        float _scale = 1f;
+        float _rotation = 0f;
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 854;
+            graphics.ApplyChanges();
+
+            _position = new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2);
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _image = Content.Load<Texture2D>("pexels-photo-278961");
+            _maskTexture = Content.Load<Texture2D>("StarMask");
+            _maskEffect = Content.Load<Effect>("Mask");
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            var elapsed = gameTime.ElapsedGameTime.Milliseconds;
+            var total = gameTime.TotalGameTime.TotalMilliseconds;
+
+            _scale = ((float)Math.Sin(elapsed * total / 10000f) * 8f);
+            _rotation = (float)(elapsed * total / 10000f);
+            
+            base.Update(gameTime);
+        }
+        
+        protected override void Draw(GameTime gameTime)
+        {            
+            GraphicsDevice.SetRenderTarget(null);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+
+            spriteBatch.Draw(_image, new Vector2(0, 0), Color.Gray); //background image
+
+            ApplyMask(_image, _maskTexture, _position, _scale, _rotation);
+            spriteBatch.Draw(_image, new Vector2(0, 0), Color.White);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        void ApplyMask(Texture2D baseTexture, Texture2D maskTexture, Vector2 maskCenter, float scale, float rotation)
+        {
+            _maskEffect.Parameters["BaseWidth"].SetValue((float)baseTexture.Width);
+            _maskEffect.Parameters["BaseHeight"].SetValue((float)baseTexture.Height);
+
+            _maskEffect.Parameters["MaskTexture"].SetValue(maskTexture);
+            _maskEffect.Parameters["MaskWidth"].SetValue((float)maskTexture.Width);
+            _maskEffect.Parameters["MaskHeight"].SetValue((float)maskTexture.Height);
+
+            _maskEffect.Parameters["MaskScale"].SetValue(scale);
+
+            _maskEffect.Parameters["MaskRotation"].SetValue(rotation);
+
+            _maskEffect.Parameters["MaskCenterX"].SetValue((float)maskCenter.X);
+            _maskEffect.Parameters["MaskCenterY"].SetValue((float)maskCenter.Y);
+
+            _maskEffect.Techniques["Mask"].Passes[0].Apply();
+        }
+    }
+}
